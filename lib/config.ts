da705a3400634e5bbf7a84fc25fb24c8e4e13027
Paul Mourer <paul.mourer@onxmaps.com>
2024-01-11 20:57:27 -0700

    More type cleanup, default config is derived type

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,11 @@
-import { DefaultClientConfig } from "../types/config";
 import { Scope } from "../types/scope";
 
-export const defaultConfig: DefaultClientConfig = {
+export const defaultConfig = {
   authorizationUri: "https://www.strava.com/api/v3/oauth/authorize",
   tokenUri: "https://www.strava.com/api/v3/oauth/token",
   revocationUri: "https://www.strava.com/api/v3/oauth/deauthorize",
   clientId: null,
   clientSecret: null,
   redirectUri: "http://localhost",
-  scopes: [Scope.Read],
-};
+  scopes: [Scope.Read] satisfies Scope[],
+} as const;
--- types/config.ts
+++ types/config.ts
@@ -1,14 +1,7 @@
 import { Scope } from "./scope";
+import { defaultConfig } from "../lib/config";
 
-export type DefaultClientConfig = {
-  authorizationUri: "https://www.strava.com/api/v3/oauth/authorize";
-  tokenUri: "https://www.strava.com/api/v3/oauth/token";
-  revocationUri: "https://www.strava.com/api/v3/oauth/deauthorize";
-  clientId: null;
-  clientSecret: null;
-  redirectUri: "http://localhost";
-  scopes: [Scope.Read];
-};
+export type DefaultClientConfig = typeof defaultConfig;
 
 export type ClientConstructorConfig = {
   authorizationUri?: string;
